e2ced4aa965b37446d905d52496fb38a70811e00
markpaw <markpaw@gmail.com>
2026-05-08 12:59:47 +0200

    restore images file

---
+++ src/assets/Images.tsx
@@ -0,0 +1,10 @@
+declare var require: any;
+import { Asset } from 'expo-asset';
+
+export const Images = {
+  //turtlelogoa: require('./logo.png'),
+  turtlelogo: require('./logo.svg'), // Assuming this was meant to be a generic logo.svg
+  usaFlag: Asset.fromModule(require('./usaFlag.svg')).uri,
+  germanyFlag: Asset.fromModule(require('./germanyFlag.svg')).uri,
+  qrCode: require('./markpawl-events.vercel.app.QR-Code.png'),
+};
